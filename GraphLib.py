4bad5717adeab9486c21122ce7eab9e3ee18f019
Rafael Torres <souza.t.rafael@gmail.com>
2019-06-04 22:05:07 -0300

    Adds graph module

---
+++ GraphLib.py
@@ -0,0 +1,68 @@
+import networkx as nx
+
+class GraphLib:
+	def __init__(self):
+		self.graph = nx.Graph()
+
+	def addEdge(self, fileA, fileB):
+		try:
+			edge = self.graph[fileA][fileB]
+			newEdgeWeight = edge['weight'] + 1
+			self.graph.add_edge(fileA, fileB, weight=newEdgeWeight)
+		except KeyError as e:
+			self.graph.add_edge(fileA, fileB, weight=1)
+
+	def getHeaviestEdges(self):
+		heaviestEdges = []
+		for edge in self.graph.edges:
+			actualEdge = (self.graph[edge[0]][edge[1]])
+			newItem = ((edge[0],edge[1]), actualEdge['weight'])
+			heaviestEdges.append(newItem)
+		return sorted(heaviestEdges, key=lambda tup: tup[1], reverse=True)
+
+	def getMostRelatedNode(self):
+		maxRelated = 0
+
+		for node in self.graph.nodes:
+			neighborCount = sum(1 for neighbor in nx.neighbors(self.graph, node))
+			if(neighborCount > maxRelated):
+				maxRelated = neighborCount
+				mostRelatedNode = node
+		return mostRelatedNode, maxRelated
+
+	def GetMostImportantFile(self):
+		removedCount = 0
+		total = 0
+		removeList = []
+		for edge in self.graph.edges:
+			actualEdge = self.graph[edge[0]][edge[1]]
+			if actualEdge['weight'] == 1:
+				removeList.append((edge[0], edge[1]))
+				removeList.append((edge[1], edge[0]))
+				removedCount += 2
+			total += 1
+		self.graph.remove_edges_from(removeList)
+		total -= removedCount
+		listSize = 0
+		weightList = []
+		for edge in self.graph.edges:
+			actualEdge = self.graph[edge[0]][edge[1]]
+			if actualEdge['weight'] in weightList:
+				continue
+			else:
+				weightList.append(actualEdge['weight'])
+				listSize += 1		
+		medianIndex = int(listSize/2)
+		median = weightList[medianIndex]
+		removedCount = 0
+		removeList = []
+		for edge in self.graph.edges:
+			actualEdge = self.graph[edge[0]][edge[1]]
+			if actualEdge['weight'] >= median:
+				removeList.append((edge[0], edge[1]))
+				removeList.append((edge[1], edge[0]))
+				removedCount += 2
+		self.graph.remove_edges_from(removeList)
+		total -= removedCount
+		mostRelatedNode = self.getMostRelatedNode()
+		return mostRelatedNode
